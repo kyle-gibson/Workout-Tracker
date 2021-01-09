2b9a8d2dfdd27c61371af47745124c415705641c
Kyle Gibson <kyle.gibson22@yahoo.com>
2021-01-09 15:33:23 -0800

    server.js added

--- server.js
+++ server.js
@@ -1,15 +1,23 @@
-const mongoose = require("mongoose");
 const express = require("express");
 const logger = require("morgan");
+const mongoose = require("mongoose");
 
-const PORT = process.env.PORT || 9000;
+const PORT = process.env.PORT || 3000;
 
 const app = express();
 
 app.use(logger("dev"));
 
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
 app.use(express.static("public"));
 
-mongoose.connect(process.env.MONGODB_URI || "mongodb:localhot/workout", {useNewUrlParser: true});
+require("./routes/html-routes")(app);
+require("./routes/api-routes")(app);
 
-require("./routes/api-routes.js")(app);
+mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/workout", { useNewUrlParser: true });
+
+app.listen(PORT, () => {
+    console.log(`App running on port ${PORT}`);
+  });
